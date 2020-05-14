d82dddc3330e957a095a25b135e5079fa6f24599
Janelle Kuhns <j3ckuhns@gmail.com>
2020-05-13 18:26:40 -0700

    added models

---
+++ .venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/janellekuhns/q4-july-se7/http_forms_cows/http_forms_cows/.venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
